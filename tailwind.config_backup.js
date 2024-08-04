88414a0edecd13fd833607bb67afdf60ed76f5b4
Talbot Lawrence <talbotlawrence75@gmail.com>
2024-08-03 22:39:23 -0500

    please

---
+++ tailwind.config_backup.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+  //BE CAREFUL WHEN YOU MOVE FILES AROUND YOU IDIOT!!!
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
